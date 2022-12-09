5f0a65bbf420b6712dc67dd42dab9a895cfc442d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-09 04:49:43 +0000

    Adding tarfile member sanitization to extractall()

--- pexsnap.py
+++ pexsnap.py
@@ -208,7 +208,26 @@ def extract_snap(snapshot_output, snapshot_input): # extract snapshot
     with alive_bar(length=20, title='Decompressing snapshot') as bar_progress:
         try:
             with tarfile.open(snapshot_input) as tar:
-                tar.extractall(path=snapshot_output)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=snapshot_output)
                 bar_progress()
         except Exception as e:
             print('Extraction failed:', e)
